11b9acb00e9d7845ca1ff625278a6ce52cdcdcb3
VT <vtciulin@mail.ru>
2023-05-27 11:04:14 +0300

    добавлен веб

---
+++ Web/Program.cs
@@ -0,0 +1,23 @@
+
+using ParserDll;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//принимает текст и вызывает метод из dll. После возвращает результат в виде json
+
+app.MapPost("/api/text", (TextRequest text) =>
+{
+    var words = new DictionaryAlgorithm().GetWordsDictionaryAsParrallel(text.Text);
+    return Results.Json(words);
+});
+
+app.Run();
+
+class TextRequest
+{
+    public string Text { get; set; }
+}
